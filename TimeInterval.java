b5f590553f3cbbef86a6fc1f9d0a6905c40fa604
Vinay320 <vinay.tomar_cs20@gla.ac.in>
2022-01-19 08:40:20 +0530

    updation

--- TimeInterval.java
+++ TimeInterval.java
@@ -11,6 +11,7 @@ public class TimeInterval {
     }
     static boolean canAttendMeeting(TimeInterval[] t1arr)
     {
+        try {
         for (int i = 0; i < t1arr.length ; i++) {
             for (int j = 0; j < t1arr.length ; j++) {
 
@@ -18,27 +19,48 @@ public class TimeInterval {
                         TimeInterval temp = t1arr[i];
                         t1arr[i] = t1arr[j];
                         t1arr[j] = temp;
-
+                    }
 
                 }
             }
-        }
+        }catch (Exception e)
+        {
+            //System.out.println(e.getMessage());
+        }int c=0;
+        try {
+
         for (int i = 0; i < t1arr.length-1 ; i++) {
-            if(t1arr[i].end>t1arr[i+1].start)
-            {
-                return false;
+            if (t1arr[i].end > t1arr[i + 1].start) {
+                c=0;
+            }
+            else {
+                c=1;
             }
 
-
         }
-        return true;
+        }catch (Exception e)
+        {
+            //System.out.println(e.getMessage());
+        }
+        return c==1;
     }
     public static void main(String[] args) {
         Scanner s=new Scanner(System.in);
         int n=s.nextInt();
+        int a1,b1;
         TimeInterval[] t1arr= new TimeInterval[n];
+        try {
+
         for (int i = 0; i <n ; i++) {
-            t1arr[i]=new TimeInterval(s.nextInt(),s.nextInt());
+            a1 = s.nextInt();
+            b1 = s.nextInt();
+            if (a1<b1) {
+                t1arr[i] = new TimeInterval(a1, b1);
+            } 
+        }
+        }catch (NullPointerException e)
+        {
+            //System.out.println(e.getMessage());
         }
         if(canAttendMeeting(t1arr))
         {
